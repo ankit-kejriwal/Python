3132888d63d8f575b0a908bd0c79a47190c7de88
ankit <ankitkejriwaal@gmail.com>
2020-04-16 18:14:20 -0400

    added second commit

--- app.py
+++ app.py
@@ -6,3 +6,20 @@ print("/____|")
 
 # variables and data types
 
+character_name = "Ankit"
+character_age = 26
+print("My name is " + character_name)
+print("I am from India")
+print("I am " +str(character_age)+" years old")
+
+# working with strings
+
+print("Python tutorial is happening")
+phrase = "ms_cs"
+print(phrase.lower())
+print(phrase.upper())
+print(phrase.isupper())
+print(len(phrase))
+print(phrase[4])
+print(phrase.index("cs"))
+print(phrase.replace("ms", "uncc"))
---
+++ variables.py
@@ -0,0 +1,9 @@
+from math import *
+print(floor(3.14))
+
+# input from user
+
+number1  = input("Enpter your number1: ")
+number2  = input("Enpter your number1: ")
+result = float(number1)+ float(number2)
+print(result)
