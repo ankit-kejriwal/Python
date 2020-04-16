14b4c8c1b3a16d0024cb8095ce4925e286e94fef
ankit <ankitkejriwaal@gmail.com>
2020-04-16 19:10:56 -0400

    added second commit

---
+++ list_tut.py
@@ -0,0 +1,12 @@
+lucky_number =[1,2,4,6,2,5]
+friends = ["ankit", "ank", "aki", "kej", "AK", "AK","kejriwal"]
+friends.remove("kej")
+friends.pop()
+print(friends.index("AK"))
+print(friends.count("AK"))
+friends.sort();
+lucky_number.sort()
+print(friends)
+reverse_number = lucky_number.copy();
+print(reverse_number)
+print(lucky_number)
--- variables.py
+++ variables.py
@@ -7,3 +7,9 @@ number1  = input("Enpter your number1: ")
 number2  = input("Enpter your number1: ")
 result = float(number1)+ float(number2)
 print(result)
+
+# list
+friends = ["a", "b", "c", "d"]
+print(friends[0])
+print(friends[3])
+print(friends[-1])
